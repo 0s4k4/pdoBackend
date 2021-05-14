473b45ead8bb5549613ecef06525be0df7827406
0s4k4 <soulbosss@gmail.com>
2021-05-13 23:56:33 -0500

    se establacen los modulos de vista que tendra la aplicacion web

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin</title>
 </head>
 <body>
-    <h1>backend</h1>
+    <?php
+    include 'modulos/header.php';
+    include 'modulos/menu.php';
+    include 'modulos/footer.php';
+
+    ?>
 </body>
 </html>
